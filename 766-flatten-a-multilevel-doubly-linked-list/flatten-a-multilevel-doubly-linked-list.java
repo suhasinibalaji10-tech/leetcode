ac7896f951af8b085b76bcc664b29d39a8138f9b
suhasinibalaji10-tech <suhasinibalaji10@gmail.com>
2026-03-10 14:27:28 +0530

    Time: 1 ms (37.06%) | Memory: 43.5 MB (54.56%) - LeetSync

---
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,25 @@
+class Solution {
+    public Node flatten(Node head) {
+        if(head==null){
+            return head;
+        }
+        Node cur=head;
+        while(cur!=null){
+            if(cur.child!=null){
+                Node next=cur.next;
+                cur.next=flatten(cur.child);
+                cur.next.prev=cur;
+                cur.child=null;
+                while(cur.next!=null){
+                    cur=cur.next;
+                }
+                if(next!=null){
+                    cur.next=next;
+                    next.prev=cur;
+                }
+            }
+            cur=cur.next;
+        }
+        return head;
+    }
+}
