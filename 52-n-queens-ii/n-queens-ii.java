1757f4beceebc638457493f71b2f05f6ed5cd75a
suhasinibalaji10-tech <suhasinibalaji10@gmail.com>
2026-03-16 15:00:02 +0530

    Time: 5 ms (11.80%) | Memory: 42.2 MB (38.83%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int totalNQueens(int n) {
+        List<List<String>> result=new ArrayList<>();
+        char[][] board=new char[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(board[i],'.');
+        }
+        place(board,0,result);
+        return result.size();
+    }
+    public void place(char[][] board,int row,List<List<String>> result){
+        if(row==board.length){
+            List<String>list =new ArrayList<>();
+            for(int i=0;i<board.length;i++){
+                list.add(new String(board[i]));
+            }
+            result.add(list);
+        }
+        for(int col=0;col<board.length;col++){
+            if(isSafe(board,row,col)){
+                board[row][col]='Q';
+                place(board,row+1,result);
+                board[row][col]='.';
+            }
+        }
+    }
+    public boolean isSafe(char[][] board,int row,int col){
+        for(int i=0;i<row;i++){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+            for(int j=0;j<board.length;j++)
+            {
+                if(board[i][j]=='Q')
+                {
+                    if(Math.abs(row-i)==Math.abs(col-j))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
